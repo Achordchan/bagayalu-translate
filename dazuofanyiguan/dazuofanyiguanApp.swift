9f329c49496f9f59c6d7f0d1de89a2eb86afee8f
Achord <achordchan@gmail.com>
2025-12-19 09:10:41 +0800

    Initial Commit

---
+++ dazuofanyiguan/dazuofanyiguanApp.swift
@@ -0,0 +1,17 @@
+//
+//  dazuofanyiguanApp.swift
+//  dazuofanyiguan
+//
+//  Created by AchordChan on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct dazuofanyiguanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dazuofanyiguanTests/dazuofanyiguanTests.swift
@@ -0,0 +1,17 @@
+//
+//  dazuofanyiguanTests.swift
+//  dazuofanyiguanTests
+//
+//  Created by AchordChan on 2025/12/19.
+//
+
+import Testing
+@testable import dazuofanyiguan
+
+struct dazuofanyiguanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
